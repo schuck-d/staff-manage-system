5f0710d17aff4f3a6a42758efd1489a7cba29735
duxin <2398568768@qq.com>
2022-09-03 16:53:13 +0800

    chore：项目初始化

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,12 @@
+import './index.css';
+
+
+function login () {
+    return (
+        <div className="login-wrap">
+            <section className="content">登录</section>
+        </div>
+    );
+}
+
+export default login
